1c2fdf181ec44fa0328f1759f079b14032b5bfcd
Pablo Alcain <pabloalcain@gmail.com>
2020-06-08 19:47:52 -0300

    Sumar y restar dos números
    Sólo sirve para números enteros, la resta está escrita como la suma con
    un número negativo

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+def sumar(x: int, y: int) -> int:
+    """
+    Esta función suma dos números enteros
+    """
+    return x + y
+
+
+def restar(x: int, y: int) -> int:
+    return sumar(x, -1 * y)
+
+
+class Cuentas:
+    pass
+
